674ca2cd43d2c3ad12f6909bc798281f2fef2925
amir <amirazizihosein@gmail.com>
2023-04-03 00:29:22 +0430

    fetch data with axios

--- src/context.jsx
+++ src/context.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useContext, useEffect } from "react"
 import { useCallback } from "react"
 import axios from "axios"
-const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s="
+const url = "https://www.thecocktaildb.com/api/json/v1/1/"
 const autoFetch = axios.create({
   baseURL: url,
-  headers: { Accept: "application/json" },
 })
 const AppContext = React.createContext()
 
@@ -12,6 +11,46 @@ const AppProvider = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true)
   const [searchTerm, setSearchTerm] = useState("a")
   const [cocktails, setCocktails] = useState([])
+  const fentchData = async () => {
+    setIsLoading(true)
+    try {
+      const { data } = await autoFetch(`search.php?s=${searchTerm}`)
+
+      if (data?.drinks) {
+        const { drinks } = data
+        const newCocktails = drinks.map((drink) => {
+          const {
+            idDrink,
+            strDrink,
+            strDrinkThumb,
+            strAlcoholic,
+            strGlass,
+          } = drink
+          return {
+            id: idDrink,
+            name: strDrink,
+            image,
+            strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          }
+        })
+        setCocktails(newCocktails)
+      } else {
+        setCocktails([])
+      }
+      setIsLoading(false)
+    } catch (error) {
+      console.log(error.response)
+      setIsLoading(false)
+    }
+  }
+  useEffect(() => {
+    fentchData()
+  }, [])
+  useEffect(() => {
+    fentchData()
+  }, [searchTerm])
   return (
     <AppContext.Provider
       value={{ isLoading, searchTerm, setSearchTerm, cocktails }}
