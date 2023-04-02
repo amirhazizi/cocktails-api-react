a08b8dcf80a21e7de0d5dfb9cf1dab693933f3ee
amir <amirazizihosein@gmail.com>
2023-04-02 23:13:57 +0430

    setup react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import React from "react"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 // import pages
-import Home from './pages/Home'
-import About from './pages/About'
-import SingleCocktail from './pages/SingleCocktail'
-import Error from './pages/Error'
+import Home from "./pages/Home"
+import About from "./pages/About"
+import SingleCocktail from "./pages/SingleCocktail"
+import Error from "./pages/Error"
+import SharedLayout from "./pages/SharedLayout"
 // import components
-import Navbar from './components/Navbar'
+import Navbar from "./components/Navbar"
 function App() {
   return (
-    <div>
-      <h2>app component</h2>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path='about' element={<About />} />
+          <Route path='coctails/:coctailID' element={<SingleCocktail />} />
+          <Route path='*' element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
